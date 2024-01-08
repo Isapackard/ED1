ffdb947e53e48e0db0cc26907d248de72aca6943
Isapackard <144940350+Isapackard@users.noreply.github.com>
2024-01-08 14:24:00 -0500

    Update variable

--- CreativeCoding/tuningwithvariables/sketch.js
+++ CreativeCoding/tuningwithvariables/sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+    
+  strokeWeight(1);
+  fill ('magenta');
+  background ("pink" )
 }
 
-function draw() {// runs in a loop 
+function draw() {
  
+  var num = 2;// variable for the number of squares
+  var sideLen = windowWidth/num;// variable for the side length of each square 
+ 
+  
+  for (var y = 0; y < windowHeight; y = y+sideLen) {  //loop to create a row squares   in the y direction
+  
+    for (var x = 0; x < windowWidth; x = x+sideLen)  {//loop to create a row squares in the x direction}
+  
+   quad
+  (x, y,
+  x + sideLen,y,
+  x + sideLen,y + sideLen,
+  x, y + sideLen
+ );//move 300 pixels in the x direction
+  
+    }
+  }
+
+} 
+
+function windowResized (){
+  resizeCanvas (windowWidth, windowHeight);
 }
+
