45cd1ced9c5b01a246aff700269ee5dc7b692596
Isapackard <144940350+Isapackard@users.noreply.github.com>
2024-01-25 10:45:39 -0500

    update web page

---
+++ sketch.js
@@ -0,0 +1,38 @@
+// adding GIF to my grid 
+
+let gif;
+
+function preload (){
+gif = loadImage ('assets/Packard800.gif');
+}
+
+function setup() {
+	// runs one time
+  createCanvas(windowWidth, windowHeight);
+    
+  
+}
+
+function draw() {
+// runs in a loop
+
+background ('pink');
+ 
+  var num = 10;// variable for the number of squares
+  var sideLen = windowWidth/num;// variable for the side length of each square 
+ 
+  
+  for (var y = 0; y < windowHeight; y = y+sideLen) {  //loop to create a row squares   in the y direction
+  
+    for (var x = 0; x < windowWidth; x = x+sideLen)  {//loop to create a row squares in the x direction}
+  
+  image (gif, x, y, sideLen, sideLen);
+  
+    }
+  }
+
+} 
+
+function windowResized (){
+  resizeCanvas (windowWidth, windowHeight);
+}
